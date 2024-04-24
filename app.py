cc37b9ac909d6d902c136ac73b939ea77307b764
gurvansh00 <118958451+gurvansh00@users.noreply.github.com>
2024-04-24 14:49:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,14 +11,27 @@ with col1:
   clock=pygame.time.Clock()
   space = pymunk.Space()
   FPS = 50
-  #class Ball():
-   # def
+  def convert_coordinates(point):
+    return int(point[0]), int(600-point[1])
+  class Ball():
+    def init_(self, x, y):
+      self.body = pymunk.Body()
+      self.body. position = x, y
+      self.shape = pymunk.Circle（self.body, 10）
+      self.shape.density = 1
+      self.shape.elasticity = 1
+    def draw(self):
+      pygame.draw.circle(display, (255,0,0), convert_coordinates(self.body.position),10
+
   
   def game():
+    ball_1 = Ball(300,300)
     while True:
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           return
+      display.fill((255,255,255))
+      ball_1.draw()
       pygame.display.update()
       clock.tick(FPS)
       space.step(1/FPS)
